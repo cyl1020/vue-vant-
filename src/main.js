9f4678c2b79afd45ef2dc4b7123a03a94a3d7b3e
cyl_g@cyl.com <cyl_g@cyl.com>
2020-12-29 11:40:44 +0800

    初始化模板

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import './utils/rem'
+import './assets/css/resize.css'
+
+import ElementUI from 'element-ui';
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="index">
+    123
+    <el-Button>123</el-Button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+};
+</script>
+<style lang="scss" scoped>
+.index {
+  color: $globalColor;
+}
+</style>

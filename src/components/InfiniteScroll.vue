7c9357233120da159960e48cd628edccba1f7f5e
cyl_g@cyl.com <cyl_g@cyl.com>
2020-12-29 15:44:06 +0800

    增加上拉加载更多组件和使用该组件的方法

--- src/components/InfiniteScroll.vue
+++ src/components/InfiniteScroll.vue
@@ -1,12 +1,8 @@
 <template>
   <div class="infinite-list-wrapper" style="overflow:auto">
-    <ul
-      class="list"
-      v-infinite-scroll="load"
-      infinite-scroll-disabled="disabled"
-    >
-      <li v-for="i in count" class="list-item" :key="i">{{ i }}</li>
-    </ul>
+    <div v-infinite-scroll="load" infinite-scroll-disabled="disabled">
+      <slot name="count" :data="count"></slot>
+    </div>
     <p v-if="loading">加载中...</p>
     <p v-if="noMore">没有更多了</p>
   </div>
---
+++ src/views/breadcrumbTest/test1.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="test1">
+    this is test1
+    <InfiniteScroll :loadingData="loadingData">
+      <template #count="{data}">
+        <p v-for="item in data" :key="item">{{item}}</p>
+      </template>
+    </InfiniteScroll>
+  </div>
+</template>
+
+<script>
+import InfiniteScroll from "../../components/InfiniteScroll";
+
+export default {
+  name: "Test1",
+  data() {
+    return {
+      count: 10,
+    };
+  },
+  components: {
+    InfiniteScroll,
+  },
+  methods: {
+    loadingData() {
+      this.count += 20;
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve({
+            count: this.count,
+            all: 100,
+          });
+          reject(30);
+        }, 2000);
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>

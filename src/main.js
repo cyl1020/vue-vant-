efffe90f1851a4948f32a09cf5c52a0ba7096a4b
cyl_g@cyl.com <cyl_g@cyl.com>
2020-12-29 11:56:06 +0800

    update

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import store from './store'
 import './utils/rem'
 import './assets/css/resize.css'
 
-import 'element-ui/lib/theme-chalk/index.css'
 import 'element-ui/lib/theme-chalk/index.css';
 import ElementUI from 'element-ui';
 Vue.use(ElementUI);

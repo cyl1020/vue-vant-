31a505c33f2d999c68d6d7cbcee820de6ca04992
cyl_g@cyl.com <cyl_g@cyl.com>
2020-12-30 13:45:05 +0800

    增加自定义单选框组件

---
+++ src/components/CustomRadio.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="custom_radio">
+    <label
+      :for="item.label"
+      class="custom_radio-label"
+      :class="{ avtiveClass: item.value == radioValue }"
+      v-for="(item, index) in radioData"
+      :key="index"
+    >
+      <input
+        class="custom_radio-input"
+        type="radio"
+        v-model="radioValue"
+        :id="item.label"
+        :value="item.value"
+        @click="selectRadio(index, $event)"
+      />
+      <p class="custom_radio-text">{{ item.value }}</p>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CustomRadio",
+  props: {
+    value: {
+      type: String,
+      default: "",
+    },
+    radioData: {
+      type: Array,
+      default: () => [],
+    },
+    noSelect: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      radioValue: this.value,
+      activeIndex: 0,
+    };
+  },
+  methods: {
+    selectRadio(index, e) {
+      this.$emit("input", e.target.value);
+      if (this.noSelect && index == this.activeIndex) {
+        this.radioValue = "";
+        this.$emit("input", this.radioValue);
+      }
+      this.activeIndex = index;
+
+      // this.$emit("input", e.target.value);
+      // if (this.noSelect && e.target.value == this.radioValue) {
+      //   this.radioValue = "";
+      //   this.$emit("input", this.radioValue);
+      // }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.custom_radio {
+  display: flex;
+
+  &-label {
+    display: flex;
+    align-items: center;
+    margin: 5px 10px;
+    border: 1px solid skyblue;
+    border-radius: 5px;
+    padding: 10px;
+  }
+
+  &-input {
+    height: 40px;
+    margin-right: 8px;
+  }
+
+  &-text {
+    height: 40px;
+    user-select: none;
+  }
+
+  .avtiveClass {
+    color: #0075ff;
+    border: 1px solid #0075ff;
+  }
+}
+</style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -3,13 +3,46 @@
     this is index
     <el-Button v-has="'index'">123</el-Button>
     <el-Button v-has="'index1'">456</el-Button>
+    <CustomRadio v-model="city" :radioData="cityData" :noSelect="true" />
+    <CustomRadio v-model="className" :radioData="classNameData" />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import CustomRadio from "../components/CustomRadio";
+
 export default {
   name: "Index",
+  components: {
+    CustomRadio,
+  },
+  data() {
+    return {
+      city: "北京",
+      cityData: [
+        {
+          label: "北京",
+          value: "北京",
+        },
+        {
+          label: "成都",
+          value: "成都",
+        },
+      ],
+      className: "计科一班",
+      classNameData: [
+        {
+          label: "计科一班",
+          value: "计科一班",
+        },
+        {
+          label: "计科二班",
+          value: "计科二班",
+        },
+      ],
+    };
+  },
 };
 </script>
 <style lang="scss" scoped>
